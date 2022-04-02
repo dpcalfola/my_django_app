7220d126a2a7295996bda4b7c9e97b058097cce6
Fola Flor <dpcalfola@gmail.com>
2022-04-03 05:51:59 +0900

    Feat > Design model
    mytasks.models.TodayTask
    Resister this model on mytasks.admin

--- mytasks/models.py
+++ mytasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TodayTasks(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    importance = models.IntegerField()
+    created_date = models.DateTimeField()
